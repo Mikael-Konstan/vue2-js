c2abf0c0b31501ff5a223d19f6ece4979e5fc8ae
huzhanzhao <1531591388@qq.com>
2019-12-23 15:03:14 +0800

    update

--- src/pages/Editor.vue
+++ src/pages/Editor.vue
@@ -3,7 +3,7 @@
     <p class="tile">wangeditor</p>
     <p>不支持IE8 无外部依赖 支持内网</p>
     <div class="Editor">
-      <wangEditor :editorContent='editorContent' @EditorMsg='getEditor'></wangEditor>
+      <wangEditor v-if="editorContentFlag" :editorContent='editorContent' @EditorMsg='getEditor'></wangEditor>
       <div class="showMsg">
         <div class="showMsg1" ref="showMsg1">{{ editorContent }}</div>
         <div class="showMsg2" ref="showMsg2">{{ editorContent }}</div>
@@ -13,21 +13,38 @@
 </template>
 
 <script>
+import axios from 'axios'
 import wangEditor from "./../components/wangEditor.vue";
 export default {
   name: "Editor",
   data() {
     return {
-      editorContent: "<p><strong>初始化 / 回显值</strong> : testPageOne</p>"
+      editorContent: "<p><strong>初始化 / 回显值</strong> : testPageOne</p>",
+      editorContentFlag: false
     };
+  },
+  created() {
+    // this.getIntMsg()
   },
   mounted() {
     // console.log(window)
+    this.getIntMsg()
   },
   components: {
     wangEditor
   },
   methods: {
+    getIntMsg() {
+      axios.post('http://localhost:7594/postTest',{
+        test: '7594'
+      }).then((res) => {
+        console.log(res)
+        this.editorContent = res.data.data
+        this.editorContentFlag = true
+      }).catch(err => {
+        console.log(err)
+      })
+    },
     getEditor(txt) {
       document.getElementsByClassName('showMsg1')[0].innerHTML = txt
       document.getElementsByClassName('showMsg2')[0].innerText = txt
--- src/pages/admin.vue
+++ src/pages/admin.vue
@@ -1,42 +1,47 @@
 <template>
-  <div class="main"  style="height: 100%">
-    <el-container  style="height: 100%">
+  <div class="main"
+       style="height: 100%">
+    <el-container style="height: 100%">
       <el-header style="height: 30px;">
         Header
       </el-header>
       <el-container>
         <el-aside width="200px">
-          <el-menu
-            class="el-menu-vertical-demo"
-            text-color="#fff"
-            background-color="rgb(84, 92, 100)"
-            :router="true"
-            :default-active="$route.path"
-            active-text-color="#ffd04b"
-          >
-            <el-menu-item index="/dashboard" v-on:click="idx">
+          <el-menu class="el-menu-vertical-demo"
+                   text-color="#fff"
+                   background-color="rgb(84, 92, 100)"
+                   :router="true"
+                   :default-active="$route.path"
+                   active-text-color="#ffd04b">
+            <el-menu-item index="/dashboard"
+                          v-on:click="idx">
               <i class="el-icon-s-home"></i>
-              <span slot="title">首页</span>
+              <span slot="title" class="navtitle">index</span>
             </el-menu-item>
-            <el-menu-item index="/Editor">
-              <i class="el-icon-location"></i>
-              <span slot="title">Editor</span>
-            </el-menu-item>
+            <el-submenu index='/Editor'>
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span class="navtitle">Editor</span>
+              </template>
+              <el-menu-item index="/Editor">
+                <span slot="title">wangEditor</span>
+              </el-menu-item>
+            </el-submenu>
             <el-menu-item index="/pageTwo">
               <i class="el-icon-menu"></i>
-              <span slot="title">pageTwo</span>
+              <span slot="title" class="navtitle">pageTwo</span>
             </el-menu-item>
             <el-menu-item index="/pageThree">
               <i class="el-icon-document"></i>
-              <span slot="title">pageThree</span>
+              <span slot="title" class="navtitle">pageThree</span>
             </el-menu-item>
             <el-menu-item index="/pageFour">
               <i class="el-icon-setting"></i>
-              <span slot="title">pageFour</span>
+              <span slot="title" class="navtitle">pageFour</span>
             </el-menu-item>
             <el-menu-item index="/pageFive">
               <i class="el-icon-setting"></i>
-              <span slot="title">pageFive</span>
+              <span slot="title" class="navtitle">pageFive</span>
             </el-menu-item>
           </el-menu>
         </el-aside>
@@ -52,7 +57,7 @@
 export default {
   name: "admin",
   methods: {
-    idx() {
+    idx () {
       this.$router.push('/dashboard')
     },
     // Editor(){
@@ -79,13 +84,17 @@ export default {
 a {
   color: #42b983;
 }
-.el-header{
+.el-header {
   background: #666;
   color: antiquewhite;
   line-height: 30px;
 }
-.el-aside{
+.el-aside {
   height: 100%;
   background: rgb(84, 92, 100);
+}
+.navtitle{
+  display: inline-block;
+  width: 60%
 }
 </style>
